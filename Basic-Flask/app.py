4b693d4744bc9797268f435342a5909f0e1f8fe7
5579096 <pawarisara.b@gmail.com>
2024-02-04 23:49:32 +0000

    Dynamic Route

--- Basic-Flask/app.py
+++ Basic-Flask/app.py
@@ -5,5 +5,19 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello Flask Framework</h1>"
 
+@app.route('/about')
+def about():
+    return "<h1>Hello About</h1>"
+
+@app.route('/admin')
+def profile():
+    return "<h1>Hello Admin</h1>"
+
+#Dynamic route
+@app.route('/user/<name>/<age>')
+def member(name, age):
+    return "<h1>Hello : {}, Age : {}</h1>".format(name, age)
+
+
 if __name__ == "__main__":
     app.run()
