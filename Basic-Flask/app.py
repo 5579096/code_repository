06d89aec014a3b5c8d63934014733b6d0a1f9094
5579096 <pawarisara.b@gmail.com>
2024-02-04 23:55:23 +0000

    Link Route to html page

--- Basic-Flask/app.py
+++ Basic-Flask/app.py
@@ -1,23 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello Flask Framework</h1>"
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
-    return "<h1>Hello About</h1>"
+    return render_template("about.html")
 
 @app.route('/admin')
 def profile():
-    return "<h1>Hello Admin</h1>"
-
-#Dynamic route
-@app.route('/user/<name>/<age>')
-def member(name, age):
-    return "<h1>Hello : {}, Age : {}</h1>".format(name, age)
+    return render_template("admin.html")
 
 
+#debug mode is true, to show error message
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
